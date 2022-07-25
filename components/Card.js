a18eb6fdaa46fd7d75f697e77a036caeba3fecc0
Kevin Palowa <kevinpalowa@gmail.com>
2022-07-25 10:23:11 +0700

    Edit Card

--- components/Card.js
+++ components/Card.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Card({ children, title, href = "/", src }) {
   return (
     <Link href={href}>
-      <a className="hover:scale-105 transition rounded-md border border-slate-500 p-3 space-y-3">
+      <a className="hover:scale-105 transition rounded-md border-4 border-slate-500 p-3 space-y-3">
         <div>
           <h3 className="font-semibold text-lg">{title}</h3>
           <p className="text-[#5e81ac]">{children}</p>
@@ -11,7 +11,7 @@ export default function Card({ children, title, href = "/", src }) {
         <div className="w-full rounded-lg">
           <img src={src} layout="fill" alt="pic" />
         </div>
-        <p>See More</p>
+        <p className="text-sm">See More...</p>
       </a>
     </Link>
   );
