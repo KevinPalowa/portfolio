c00550ef636182f52dbc5b353629a071d3f62f27
Kevin Palowa <kevinpalowa@gmail.com>
2022-10-30 13:13:27 +0700

    Add error page

---
+++ pages/404.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import Layout from "../components/Layout";
+
+function error() {
+  return (
+    <Layout title="Page not found">
+      <h2 className="text-3xl font-bold">404 - Page Not Found </h2>
+      <p className="text-slate-400 mt-3">Why this happen?</p>
+      <p className="text-slate-400">
+        Maybe you spelled url wrong, please check the url.
+      </p>
+      <Link href="/">
+        <a className="font-bold p-3 bg-gray-700 rounded-md inline-block mt-5">
+          Back to home
+        </a>
+      </Link>
+    </Layout>
+  );
+}
+
+export default error;
